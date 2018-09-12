fcf00a731d855530a57563b70095787a302f43c7
Harlock123 <harlock123@gmail.com>
2018-09-12 16:06:10 -0400

    Added a custom control for My Team

---
+++ PJsPlayhouse/PJsPlayhouse/Controls/TeamMember.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace PJsPlayhouse.Controls
+{
+    public partial class TeamMember : Xamarin.Forms.Grid
+    {
+        public TeamMember()
+        {
+            InitializeComponent();
+        }
+
+        public TeamMember(string PT, string TT, string TD)
+        {
+            InitializeComponent();
+            PicText = PT;
+            TeamMemberTitleText = TT;
+            TeamMemberDescriptionText = TD;
+        }
+
+        public string PicText
+        {
+            //get;
+            set { PicButton.Text = value; 
+                switch (value.ToUpper())
+                {
+                    case "DOC":
+                        PicButton.BackgroundColor = Color.Green;
+                        break;
+                    case "FAM":
+                        PicButton.BackgroundColor = Color.Blue;
+                        break;
+                    case "COW":
+                        PicButton.BackgroundColor = Color.DarkOrange;
+                        break;
+                    default:
+                        PicButton.BackgroundColor = Color.Yellow;
+                        break;
+                }
+
+            
+            }
+        } 
+
+        public string TeamMemberTitleText
+        {
+            //get;
+            set { TeamMemberTitle.Text = value; }
+        } 
+
+        public string TeamMemberDescriptionText
+        {
+            //get;
+            set { TeamMemberDescription.Text = value; }
+        } 
+
+    }
+}
--- PJsPlayhouse/PJsPlayhouse/MyTeamPage.xaml.cs
+++ PJsPlayhouse/PJsPlayhouse/MyTeamPage.xaml.cs
@@ -12,6 +12,21 @@ namespace PJsPlayhouse
         public MyTeamPage()
         {
             InitializeComponent();
+
+            TheList.Children.Add(new Controls.TeamMember("Doc", "Dr Drew Carey", "Endrocrynologist (Sugar Doc)"));
+            TheList.Children.Add(new Controls.TeamMember("Doc", "Dr Gary King", "Primary Care Physician"));
+            TheList.Children.Add(new Controls.TeamMember("Doc", "Dr Joseph Wyllie", "Cardiologyst"));
+            TheList.Children.Add(new Controls.TeamMember("Fam", "Sally Watson", "Wife"));
+            TheList.Children.Add(new Controls.TeamMember("Fam", "Kathleen Sherman", "Mother In Law"));
+            TheList.Children.Add(new Controls.TeamMember("Fam", "Todd Watson", "Brother"));
+            TheList.Children.Add(new Controls.TeamMember("Fam", "Kimberly Hearn", "Sister"));
+            TheList.Children.Add(new Controls.TeamMember("Fam", "Robert Lonnie Watson", "Son"));
+            TheList.Children.Add(new Controls.TeamMember("Fam", "Michael Mitchell Watson", "Son"));
+            TheList.Children.Add(new Controls.TeamMember("Cow", "Jennifer Feeney", ""));
+            TheList.Children.Add(new Controls.TeamMember("Cow", "Cheryl Bellucci", ""));
+            TheList.Children.Add(new Controls.TeamMember("Cow", "Keith Inman", ""));
+            TheList.Children.Add(new Controls.TeamMember("Cow", "Lawrence Reedy", ""));
+          
         }
 
         async void Handle_Clicked(object sender, System.EventArgs e)
