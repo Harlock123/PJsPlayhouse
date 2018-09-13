96d330c97534e7252b724805b350bd19f22c58ea
Harlock123 <harlock123@gmail.com>
2018-09-13 16:52:14 -0400

    Some changes to My Messages

--- PJsPlayhouse/PJsPlayhouse/MyMessages.xaml.cs
+++ PJsPlayhouse/PJsPlayhouse/MyMessages.xaml.cs
@@ -19,39 +19,11 @@ namespace PJsPlayhouse
             TheMessageList.Children.Add(new Controls.MemberMessage(DateTime.Now,
                 "Dr King", "Your annual checkup appointment needs to be made. Call our office at 111-222-3333 and make that appointment, see you soon."));
             TheMessageList.Children.Add(new Controls.MemberMessage(DateTime.Now,
-                "Dr King", "Your annual checkup appointment needs to be made. Call our office at 111-222-3333 and make that appointment, see you soon."));
+                "Sally Watson", "The doctors office called and left a message about your annual checkup. You need to call and make an appointment"));
             TheMessageList.Children.Add(new Controls.MemberMessage(DateTime.Now,
-                            "Dr King", "Your annual checkup appointment needs to be made. Call our office at 111-222-3333 and make that appointment, see you soon."));
+                            "Todd Watson", "I wanted to ask you what you Doctors name was. The one you go to that helped you get your sugar undercontrol"));
             TheMessageList.Children.Add(new Controls.MemberMessage(DateTime.Now,
-                            "Dr King", "Your annual checkup appointment needs to be made. Call our office at 111-222-3333 and make that appointment, see you soon."));
-            TheMessageList.Children.Add(new Controls.MemberMessage(DateTime.Now,
-                            "Dr King", "Your annual checkup appointment needs to be made. Call our office at 111-222-3333 and make that appointment, see you soon."));
-            TheMessageList.Children.Add(new Controls.MemberMessage(DateTime.Now,
-                            "Dr King", "Your annual checkup appointment needs to be made. Call our office at 111-222-3333 and make that appointment, see you soon."));
-            TheMessageList.Children.Add(new Controls.MemberMessage(DateTime.Now,
-                            "Dr King", "Your annual checkup appointment needs to be made. Call our office at 111-222-3333 and make that appointment, see you soon."));
-            TheMessageList.Children.Add(new Controls.MemberMessage(DateTime.Now,
-                            "Dr King", "Your annual checkup appointment needs to be made. Call our office at 111-222-3333 and make that appointment, see you soon."));
-            TheMessageList.Children.Add(new Controls.MemberMessage(DateTime.Now,
-                            "Dr King", "Your annual checkup appointment needs to be made. Call our office at 111-222-3333 and make that appointment, see you soon."));
-            TheMessageList.Children.Add(new Controls.MemberMessage(DateTime.Now,
-                            "Dr King", "Your annual checkup appointment needs to be made. Call our office at 111-222-3333 and make that appointment, see you soon."));
-            TheMessageList.Children.Add(new Controls.MemberMessage(DateTime.Now,
-                            "Dr King", "Your annual checkup appointment needs to be made. Call our office at 111-222-3333 and make that appointment, see you soon."));
-            TheMessageList.Children.Add(new Controls.MemberMessage(DateTime.Now,
-                            "Dr King", "Your annual checkup appointment needs to be made. Call our office at 111-222-3333 and make that appointment, see you soon."));
-            TheMessageList.Children.Add(new Controls.MemberMessage(DateTime.Now,
-                            "Dr King", "Your annual checkup appointment needs to be made. Call our office at 111-222-3333 and make that appointment, see you soon."));
-            TheMessageList.Children.Add(new Controls.MemberMessage(DateTime.Now,
-                            "Dr King", "Your annual checkup appointment needs to be made. Call our office at 111-222-3333 and make that appointment, see you soon."));
-            TheMessageList.Children.Add(new Controls.MemberMessage(DateTime.Now,
-                            "Dr King", "Your annual checkup appointment needs to be made. Call our office at 111-222-3333 and make that appointment, see you soon."));
-            TheMessageList.Children.Add(new Controls.MemberMessage(DateTime.Now,
-                            "Dr King", "Your annual checkup appointment needs to be made. Call our office at 111-222-3333 and make that appointment, see you soon."));
-            TheMessageList.Children.Add(new Controls.MemberMessage(DateTime.Now,
-                            "Dr King", "Your annual checkup appointment needs to be made. Call our office at 111-222-3333 and make that appointment, see you soon."));
-            TheMessageList.Children.Add(new Controls.MemberMessage(DateTime.Now,
-                            "Dr King", "Your annual checkup appointment needs to be made. Call our office at 111-222-3333 and make that appointment, see you soon."));
+                            "Dr Carey", "We need to get you into the office for a  screening that may have you eligible for a new sugar medication that may be less expensive for you in the long run. Please call and make an appointment..."));
 
 
         }
